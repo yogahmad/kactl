3b89ed1a371d8e7ac71f10c0b5d4e553cd0398dd
Yusuf <yfsholeh@gmail.com>
2018-11-02 10:36:31 +0700

    added MCMF

--- content/graph/MinCostMaxFlow.h
+++ content/graph/MinCostMaxFlow.h
@@ -2,92 +2,80 @@
  * Author: Stanford
  * Date: Unknown
  * Source: Stanford Notebook
- * Description: Min-cost max-flow. cap[i][j] != cap[j][i] is allowed; double edges are not.
- *  If costs can be negative, call setpi before maxflow, but note that negative cost cycles are not supported.
- *  To obtain the actual flow, look at positive values only.
+ * Description: Min-cost max-flow. 
+ * Not handling negative cycle. When there is negative cycle, there is no answer for MCMF.
  * Status: Tested on kattis mincostmaxflow
  * Time: Approximately O(E^2)
  */
 #pragma once
 
-#include <bits/extc++.h> /** keep-include */
-
-const ll INF = numeric_limits<ll>::max() / 4;
-typedef vector<ll> VL;
-
-struct MCMF {
-	int N;
-	vector<vi> ed, red;
-	vector<VL> cap, flow, cost;
-	vi seen;
-	VL dist, pi;
-	vector<pii> par;
-
-	MCMF(int N) :
-		N(N), ed(N), red(N), cap(N, VL(N)), flow(cap), cost(cap),
-		seen(N), dist(N), pi(N), par(N) {}
-
-	void addEdge(int from, int to, ll cap, ll cost) {
-		this->cap[from][to] = cap;
-		this->cost[from][to] = cost;
-		ed[from].push_back(to);
-		red[to].push_back(from);
-	}
-
-	void path(int s) {
-		fill(all(seen), 0);
-		fill(all(dist), INF);
-		dist[s] = 0; ll di;
-
-		__gnu_pbds::priority_queue<pair<ll, int>> q;
-		vector<decltype(q)::point_iterator> its(N);
-		q.push({0, s});
-
-		auto relax = [&](int i, ll cap, ll cost, int dir) {
-			ll val = di - pi[i] + cost;
-			if (cap && val < dist[i]) {
-				dist[i] = val;
-				par[i] = {s, dir};
-				if (its[i] == q.end()) its[i] = q.push({-dist[i], i});
-				else q.modify(its[i], {-dist[i], i});
-			}
-		};
-
-		while (!q.empty()) {
-			s = q.top().second; q.pop();
-			seen[s] = 1; di = dist[s] + pi[s];
-			trav(i, ed[s]) if (!seen[i])
-				relax(i, cap[s][i] - flow[s][i], cost[s][i], 1);
-			trav(i, red[s]) if (!seen[i])
-				relax(i, flow[i][s], -cost[i][s], 0);
-		}
-		rep(i,0,N) pi[i] = min(pi[i] + dist[i], INF);
-	}
-
-	pair<ll, ll> maxflow(int s, int t) {
-		ll totflow = 0, totcost = 0;
-		while (path(s), seen[t]) {
-			ll fl = INF;
-			for (int p,r,x = t; tie(p,r) = par[x], x != s; x = p)
-				fl = min(fl, r ? cap[p][x] - flow[p][x] : flow[x][p]);
-			totflow += fl;
-			for (int p,r,x = t; tie(p,r) = par[x], x != s; x = p)
-				if (r) flow[p][x] += fl;
-				else flow[x][p] -= fl;
-		}
-		rep(i,0,N) rep(j,0,N) totcost += cost[i][j] * flow[i][j];
-		return {totflow, totcost};
-	}
-
-	// If some costs can be negative, call this before maxflow:
-	void setpi(int s) { // (otherwise, leave this out)
-		fill(all(pi), INF); pi[s] = 0;
-		int it = N, ch = 1; ll v;
-		while (ch-- && it--)
-			rep(i,0,N) if (pi[i] != INF)
-				trav(to, ed[i]) if (cap[i][to])
-					if ((v = pi[i] + cost[i][to]) < pi[to])
-						pi[to] = v, ch = 1;
-		assert(it >= 0); // negative cost cycle
-	}
+struct edge{
+	int to, rev;
+	int flow, cap;
+	int cost;
 };
+vector<edge> G[500];
+inline void add(int s, int t, int capa, int costs) {
+	edge a = {t, G[t].size(), 0, capa,  costs};
+	edge b = {s, G[s].size(), 0,    0, -costs};
+	G[s].push_back(a);
+	G[t].push_back(b);
+}
+inline bool SPFA() {
+	for(int i = 0; i <= sink; i++) dist[i] = INF, flag[i] = false, bt[i] = -1, idx[i] = -1;
+	dist[source] = 0;
+	queue<int> q;
+	q.push(source);
+	flag[source] = true;
+	while(!q.empty()) {
+		int now = q.front();
+		q.pop();
+		flag[now] = false;
+		int size = G[now].size();
+		for(int i = 0; i < size; i++) {
+			int to = G[now][i].to;
+			int cost = G[now][i].cost;
+			int capa = G[now][i].cap;	
+			if (capa > 0 && dist[to] > dist[now] + cost) {
+				dist[to] = dist[now] + cost;
+				bt[to] = now;
+				idx[to] = i;
+				if (!flag[to]) {
+					flag[to] = true;
+					q.push(to);
+				}
+			}
+		}
+	}
+	return bt[sink] != -1;
+}
+ 
+pair<int,int> MCMF() {
+	pair<int,int> res; res.first = 0, res.second = 0;
+	while(true) {
+		if (!SPFA()) break;
+		int mins = INF;
+		int ptr = sink;
+		int total = 0;
+		while(ptr != source) {
+			int from = bt[ptr];
+			int id = idx[ptr];
+			if (G[from][id].cap < mins) 
+				mins = G[from][id].cap;
+			total += G[from][id].cost;
+			ptr = from;
+		}
+		res.first += mins;
+		res.second += total * mins;
+		ptr = sink;
+		while(ptr != source) {
+			int from = bt[ptr];
+			int id = idx[ptr];
+			int rev = G[from][id].rev;
+			G[from][id].cap -= mins;
+			G[ptr][rev].cap += mins;
+			ptr = from;
+		}
+	}
+	return res;
+}
