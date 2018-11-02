4b505d37cbba4143c36e2244967abad9592bc8e3
Yusuf <yfsholeh@gmail.com>
2018-11-02 11:20:32 +0700

    added eulerian walk

--- content/graph/EulerWalk.h
+++ content/graph/EulerWalk.h
@@ -3,37 +3,32 @@
  * Date: 2009-04-14
  * License: CC0
  * Source: folklore
- * Description: Eulerian undirected/directed path/cycle algorithm. Returns a list of nodes in the Eulerian path/cycle with src at both start and end, or
- *  empty list if no cycle/path exists. To get edge indices back, also put it->second in s (and then ret).
+ * Description: Eulerian undirected/directed path/cycle algorithm.
+ * Usage: 
+ * For eulerian path, should pass cur with odd degree to eulerian().
  * Time: O(E) where E is the number of edges.
  * Status: tested
  */
 #pragma once
 
-struct V {
-	vector<pii> outs; // (dest, edge index)
-	int nins = 0;
-};
-
-vi euler_walk(vector<V>& nodes, int nedges, int src=0) {
-	int c = 0;
-	trav(n, nodes) c += abs(n.nins - sz(n.outs));
-	if (c > 2) return {};
-	vector<vector<pii>::iterator> its;
-	trav(n, nodes)
-		its.push_back(n.outs.begin());
-	vector<bool> eu(nedges);
-	vi ret, s = {src};
-	while(!s.empty()) {
-		int x = s.back();
-		auto& it = its[x], end = nodes[x].outs.end();
-		while(it != end && eu[it->second]) ++it;
-		if(it == end) { ret.push_back(x); s.pop_back(); }
-		else { s.push_back(it->first); eu[it->second] = true; }
+void eulerian(int cur){
+	stack<int> st;
+	vector<int> ans;
+	st.push(cur);
+	//V is multiset
+	while(!st.empty()){
+		int cur = st.top();
+		if(V[cur].size()){
+			auto it = V[cur].begin();
+			st.push(*it);
+			V[cur].erase(it);	
+			//use this for bidirectional graph
+			//if(V[*it].count(cur)){
+			//	V[*it].erase(V[*it].find(cur));
+			//}
+		}else{
+			ans.pb(cur);
+			st.pop();
+		}
 	}
-	if(sz(ret) != nedges+1)
-		ret.clear(); // No Eulerian cycles/paths.
-	// else, non-cycle if ret.front() != ret.back()
-	reverse(all(ret));
-	return ret;
 }
