cf28f65aa2dbd31734b7a18a2c6134507c1f7ac0
Yusuf <yfsholeh@gmail.com>
2018-11-25 11:27:01 +0700

    changed KMP

--- content/strings/KMP.h
+++ content/strings/KMP.h
@@ -1,26 +1,25 @@
 /**
  * Author: Johan Sannemo
  * License: CC0
- * Description: pi[x] computes the length of the longest prefix of s that ends at x, other than s[0...x] itself (abacaba -> 0010123).
+ * Description: pi[x] computes the length of the longest prefix of t that ends at x (abacaba -> 0010123).
  * Can be used to find all occurrences of a string.
  * Time: O(n)
  * Status: Tested on Kattis, stringmatching
  */
 #pragma once
 
-vi pi(const string& s) {
-	vi p(sz(s));
-	rep(i,1,sz(s)) {
-		int g = p[i-1];
-		while (g && s[i] != s[g]) g = p[g-1];
-		p[i] = g + (s[i] == s[g]);
-	}
-	return p;
-}
-
-vi match(const string& s, const string& pat) {
-	vi p = pi(pat + '\0' + s), res;
-	rep(i,sz(p)-sz(s),sz(p))
-		if (p[i] == sz(pat)) res.push_back(i - 2 * sz(pat));
-	return res;
+int kmp(const string &T, const string &P) {
+  if (P.empty()) return 0;
+  vector<int> pi(P.size(), 0);
+  for (int i = 1, k = 0; i < P.size(); ++i) {
+    while (k && P[k] != P[i]) k = pi[k - 1];
+    if (P[k] == P[i]) ++k;
+    pi[i] = k;
+  }
+  for (int i = 0, k = 0; i < T.size(); ++i) {
+    while (k && P[k] != T[i]) k = pi[k - 1];
+    if (P[k] == T[i]) ++k;
+    if (k == P.size()) return i - k + 1;
+  }
+  return -1;
 }
