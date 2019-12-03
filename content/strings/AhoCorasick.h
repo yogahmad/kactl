6812820fff1c13ffeffa5d14fe74b3cc81699b6d
yusufsholeh <yfsholeh@gmail.com>
2019-12-03 18:31:43 +0700

    Change aho corasick

--- content/strings/AhoCorasick.h
+++ content/strings/AhoCorasick.h
@@ -11,117 +11,66 @@
  */
 #pragma once
 
-#include<bits/stdc++.h> 
-const int NALPHABET = 26;
-struct Node {
-  Node** children, go;
-  bool leaf;
-  char charToParent;
-  Node* parent, suffLink, dictSuffLink;
-  int count, value;
+const int N = 1e5 + 5;
  
-  Node(){
-    children = new Node*[NALPHABET];
-    go = new Node*[NALPHABET];
-    for(int i = 0; i < NALPHABET;++i){
-      children[i] = go[i] = NULL;
-    }
-    parent = suffLink = dictSuffLink = NULL;
-    leaf = false;
-    count = 0;
-  }
+struct AhoCorasick {
+	int trie[N][26];
+	int fail[N];
+	int saiz;
+ 
+	AhoCorasick() {
+		memset(trie[0],-1,sizeof trie[0]);
+		saiz = 0;
+	}
+ 
+	void add(string str) {
+		int cur = 0;
+		for(int i = 0 ; i < str.length() ; i++) {
+			//checkChar(str[i]);
+			int nex = str[i] - 'a';
+			if(trie[cur][nex] == -1) {
+				trie[cur][nex] = ++saiz;
+				memset(trie[saiz],-1,sizeof trie[saiz]);
+			}
+			cur = trie[cur][nex];
+		}
+	}
+ 
+	void build() {
+		queue<int> q;
+		fail[0] = 0;
+ 
+		for(int i = 0 ; i < 26 ; i++)
+			if(trie[0][i] == -1)
+				trie[0][i] = 0;
+			else {
+				int nex = trie[0][i];
+				fail[nex] = 0;
+				q.push(nex);
+			}
+ 
+		while(!q.empty()) {
+			int now = q.front();
+			q.pop();
+ 
+			for(int i = 0 ; i < 26 ; i++)
+				if(trie[now][i] == -1)
+					trie[now][i] = trie[fail[now]][i];
+				else {
+					int nex = trie[now][i];
+					fail[nex] = trie[fail[now]][i];
+					q.push(nex);
+				}
+		}	
+	}
+ 
+	int getIndex(string str) {
+		int cur = 0;
+		for(int i = 0 ; i < str.length() ; i++) {
+			//checkChar(str[i]);
+			cur = trie[cur][str[i] - 'a'];
+		}
+		return cur;
+	}
+ 
 };
- 
-Node* createRoot() {
-  Node* node = new Node();
-  node->suffLink = node;
-  return node;
-}
- 
-void addString(Node* node, const string& s, int value =-1) {
-  for(int i = 0; i < s.length(); ++i){
-    int c = s[i] - 'a';
-    if(node->children[c] == NULL){
-      Node* n = new Node();
-      n->parent = node;
-      n->charToParent = s[i];
-      node->children[c] = n;
-    }
-    node = node->children[c];
-  }
-  node->leaf = true;
-  node->count++;
-  node->value = value;
-}
- 
-Node* suffLink(Node* node);
-Node* dictSuffLink(Node* node);
-Node* go(Node* node, char ch);
-int calc(Node* node);
- 
-Node* suffLink(Node* node) {
-  if (node->suffLink == NULL){
-    if (node->parent->parent == NULL){
-      node->suffLink = node->parent;
-    } else {
-      node->suffLink = go(suffLink(node->parent),node->charToParent);
-    }
-  }
-  return node->suffLink;
-}
- 
-Node* dictSuffLink(Node* node) {
-  if(node->dictSuffLink == NULL){
-    Node* n = suffLink(node);
-    if (node == n){
-      node->dictSuffLink = node;
-    } else {
-      while (!n->leaf && n->parent != NULL){
-        n = dictSuffLink(n);
-      }
-      node->dictSuffLink = n;
-    }
-  }
-  return node->dictSuffLink;
-}
- 
-Node* go(Node* node, char ch) {
-  int c = ch -'a';
-  if (node->go[c] == NULL){
-    if (node->children[c] != NULL) {
-      node->go[c]= node->children[c];
-    } else {
-      node->go[c]= node->parent == NULL? node : go(suffLink(node), ch);
-    }
-  }
-  return node->go[c];
-}
-
-int calc(Node* node) {
-  if (node->parent == NULL) {
-    return 0;
-  } else {
-    return node->count + calc(dictSuffLink(node));
-  }
-}
- 
-int main() {
-  Node* root = createRoot();
-  addString(root,"a",0);
-  addString(root,"aa",1);
-  addString(root,"abc",2);
- 
-  string s("abcaadc");
-  Node* node = root;
-  for (int i = 0; i < s.length(); ++i){
-    node = go(node, s[i]);
-    Node* temp = node;
-    while (temp != root) {
-      if (temp->leaf) {
-        printf("string (%d) occurs at position %d\n", temp->value, i);
-      }
-      temp = dictSuffLink(temp);
-    }
-  }
-  return 0;
-}
